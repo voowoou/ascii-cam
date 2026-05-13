b42f85fa243ffce2e8825542b32be783b14fa903
voowoou <voowoou@gmail.com>
2026-05-13 15:52:16 +0700

    feat: add canvas that displays fetched web camera video

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,93 @@
 import { render } from "preact";
+import { useEffect, useRef, useState } from "preact/hooks";
 
-import preactLogo from "./assets/preact.svg";
 import "./style.css";
 
+const CONSTRAINTS: MediaStreamConstraints = {
+  audio: false,
+  video: {
+    width: 800,
+    height: 600,
+    facingMode: "user",
+  },
+};
+
 export function App() {
-  return <div>Hello</div>;
+  const [err, setErr] = useState<DOMException | null>(null);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const getImgData = () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+
+    if (video && canvas) {
+      const ctx = canvas.getContext("2d", { willReadFrequently: true });
+
+      if (ctx) {
+        const width = 150;
+
+        canvas.width = width;
+        canvas.height = width * (video.height / video.width);
+
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      }
+
+      requestAnimationFrame(getImgData);
+    }
+  };
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia(CONSTRAINTS)
+      .then((s) => setStream(s))
+      .catch((err) => setErr(err));
+  }, []);
+
+  useEffect(() => {
+    if (videoRef.current && stream) videoRef.current.srcObject = stream;
+  }, [stream]);
+
+  if (!stream)
+    return (
+      <div className="errContainer">
+        <h2>Please turn on your web camera</h2>
+      </div>
+    );
+
+  if (err)
+    return (
+      <div className="errContainer">
+        <h2>{err.name}</h2>
+        <p>{err.message}</p>
+      </div>
+    );
+
+  return (
+    <div>
+      <video
+        ref={videoRef}
+        onPlay={getImgData}
+        height={600}
+        width={800}
+        autoPlay
+        playsInline
+        style={{ display: "none" }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          width: "100%",
+          maxWidth: "800px",
+          height: "auto",
+          imageRendering: "pixelated",
+        }}
+      ></canvas>
+    </div>
+  );
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById("app")!);
